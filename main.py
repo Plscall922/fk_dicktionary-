987e435719b1189e871cfe373004f0ff55633398
Plscall922 <40308170+Plscall922@users.noreply.github.com>
2023-05-22 19:26:38 +1000

    HUI!

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+fucking_dictinaty = {'dick': ["suka", "bliat"], "hui": "poshol_nach"}
 
+for bliat, suka in fucking_dictinaty.items():
+    print(bliat)
+    if isinstance(suka, list):
+        for pes in suka:
+            print(pes)
+    else:
+        print(suka)
+        print("ujebok")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for value in fucking_dictinaty.values():
+    print(value)
